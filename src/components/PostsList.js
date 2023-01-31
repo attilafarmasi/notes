854fa0c5585f8e9750566f0ddfd6a9c887443801
Farmasi Attila <attila.farmasi@dpdgroup.com>
2023-01-31 15:01:34 +0100

    Handling side effects with useEffect()

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import classes from "./PostsList.module.css";
 import Post from "./Post";
@@ -7,6 +7,16 @@ import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
