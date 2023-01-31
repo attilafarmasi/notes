03d108f9a1d4a31035be90059433bafe4a45412b
Farmasi Attila <attila.farmasi@dpdgroup.com>
2023-01-31 11:12:37 +0100

    Preparing the App for state management

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,12 +1,16 @@
 import classes from "./PostsList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
